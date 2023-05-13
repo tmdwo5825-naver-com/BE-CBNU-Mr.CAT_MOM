b5af0deffe6ac135fc28ff3dee8a787a86bf7515
chceol <mine3386@naver.com>
2023-05-13 19:59:02 +0900

    feature: database의 config파일을 설정한다. - config.ini파일 추가 및 config.ini를 이용한 db연결

--- database/db.py
+++ database/db.py
@@ -1,10 +1,24 @@
+import configparser
+import os
+from pathlib import Path
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-import json
-import os
 
-DB_URL = "mysql+pymysql://root:1234@localhost:3306/cat_mom"
+path = Path(__file__)
+ROOT_DIR = path.parent.absolute()
+config_path = os.path.join(ROOT_DIR, "config.ini")
+
+config = configparser.ConfigParser()
+config.read(config_path)
+
+user = config.get('mysql', 'user')
+password = config.get('mysql', 'password')
+host = config.get('mysql', 'host')
+port = config.get('mysql', 'port')
+database = config.get('mysql', 'database')
+
+DB_URL = "mysql+pymysql://%s:%s@%s:%s/%s" % (user, password, host, port, database)
 
 engine = create_engine(DB_URL)
 
