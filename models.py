872ca6d34a0d4b57a503d918cf2ae65df5ebdb04
chceol <mine3386@naver.com>
2023-05-13 19:19:02 +0900

    models.py의 오타 수정

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, VARCHAR, DateTime, text
 from .database import Base
 
 class Cat(Base):
-    __tablename__ = "cat_mom"
+    __tablename__ = "Cat_mom"
     geo_id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     created_at = Column(DateTime, server_default=text('CURRENT_TIMESTAMP'))
     storage_URL = Column(VARCHAR(500))
