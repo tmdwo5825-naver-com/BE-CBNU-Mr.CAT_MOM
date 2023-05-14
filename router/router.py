897a5e25d78667bfc648280cb81232bc3d260413
chceol <mine3386@naver.com>
2023-05-15 00:33:37 +0900

    get요청을 처리하는 get_content router 개발

--- router/router.py
+++ router/router.py
@@ -21,9 +21,9 @@ def create_content(request: schemas.CatCreate, db: Session = Depends(get_db)):
     crud.create_cat(db, request, presigned_url)
 
 
-@router.get("/", response_model= list[schemas.Test])
-def get_dummy(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    response = crud.get_cat(db= db, skip= skip, limit= limit)
+@router.get("/", response_model= list[schemas.CatResponse])
+def get_content(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    response = crud.get_cat(db=db, skip=skip, limit=limit)
     if response is None:
-        raise HTTPException(status_code=404, detail= "dummy not found")
+        raise HTTPException(status_code=404, detail= "content not found")
     return response
