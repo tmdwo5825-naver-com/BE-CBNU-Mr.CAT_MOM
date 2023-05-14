74b196631f2d18c24c9e7ee39c2c4f1ff156fd0f
chceol <mine3386@naver.com>
2023-05-15 02:20:10 +0900

    refactor: orm model과 router기능을 수정한다. - get요청이 들어올 때 마다 url을 생성하여 함께 반환하도록 한다.

--- crud.py
+++ crud.py
@@ -2,8 +2,9 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
-def create_cat(db: Session, cat: schemas.CatCreate, presigned_url: str = None):
-    db_cat = models.Cat(created_at=cat.created_at, x=cat.x, y=cat.y, storage_URL=presigned_url, comment=cat.comment)
+
+def create_cat(db: Session, cat: schemas.CatCreate, obj_name: str = None):
+    db_cat = models.Cat(created_at=cat.created_at, x=cat.x, y=cat.y, obj_name=obj_name, comment=cat.comment)
     db.add(db_cat)
     db.commit()
     db.refresh(db_cat)
--- schemas.py
+++ schemas.py
@@ -10,12 +10,14 @@ class CatCreate(BaseModel):
     x: int
     y: int
 
+
 class CatResponse(BaseModel):
     x: int
     y: int
     created_at: str
     comment: str
     url: str
+    obj_name: str
 
     geo_id: int
 
