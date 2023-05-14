61d1de9ef9d6969ef04c4f226a04d042abc99f6b
chceol <mine3386@naver.com>
2023-05-15 00:29:18 +0900

    s3에 파일 업로드 및 presigned_url을 생성받는 기능 추가

--- router/router.py
+++ router/router.py
@@ -4,16 +4,20 @@ from sqlalchemy.orm import Session
 from .. common.dependencies import get_db
 from .. import schemas, crud
 
+from .. aws.s3 import upload_file, create_presigned_url
+
 router = APIRouter()
 
 
 @router.post("/content-create/")
-def create_dummy(request: schemas.CatCreate, db: Session = Depends(get_db) ):
+def create_content(request: schemas.CatCreate, db: Session = Depends(get_db)):
     if not request.image:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="No image found"
         )
+    obj_name = upload_file(request.image)
+    presigned_url = create_presigned_url("s3-cbnu-cat-mom", obj_name)
     crud.create_cat(db,request)
 
 
