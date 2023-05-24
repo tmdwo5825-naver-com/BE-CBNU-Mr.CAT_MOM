8927d7c7defd57facfbd8698f813eb57d4df6545
chceol <mine3386@naver.com>
2023-05-24 15:29:44 +0900

    create_24h_content함수 정의

--- crud/crud_cat.py
+++ crud/crud_cat.py
@@ -1,6 +1,22 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from app import models, schemas
+
+
+class CrudCat():
+    def create_24h_content(self, db: Session, cat_in: schemas.CatCreate):
+        db_cat = models.Cat(
+            x=cat_in.x,
+            y=cat_in.y,
+            image_url=cat_in.image_url,
+            comment=cat_in.comment
+        )
+        db.add(db_cat)
+        db.commit()
+        db.refresh(db_cat)
+        return db
+
+
 
 
 def create_cat(db: Session, cat: schemas.CatCreate):
